bc93bd6c55daf876d06c84f6f238f79c490df6a8
Andrei Constantinescu <andrei.c@icloud.com>
2014-10-15 02:09:00 +0300

    Add overlay over backgound.

--- app/js/pages/home.js
+++ app/js/pages/home.js
@@ -7,11 +7,21 @@ var templates = require('../lib/templates');
 module.exports = View.extend({
   pageTitle: 'Balul Bobocilor | Home',
   template: templates.pages.home,
+  events: {
+    'click .chevron': 'hadleChevronClick'
+  },
+
   render: function () {
     var self = this;
 
     self.$el.html(self.template());
 
     return self;
+  },
+
+  handleChvronClick: function () {
+    $('body').animate({
+      scrollTop: this.$('.asmi').offset().top - 50
+    }, 200);
   }
 });
