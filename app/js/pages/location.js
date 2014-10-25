305a60124f3ecbb93c28825be35838fe318edee7
Andrei Constantinescu <andrei.c@icloud.com>
2014-10-26 00:17:30 +0300

    Add Location page.

--- app/js/pages/location.js
+++ app/js/pages/location.js
@@ -3,6 +3,7 @@
 var Backbone = require('../shims/backbone');
 var View = Backbone.View;
 var templates = require('../lib/templates');
+var $ = require('../shims/jquery');
 
 module.exports = View.extend({
   pageTitle: 'Balul Bobocilor | Location',
@@ -13,6 +14,26 @@ module.exports = View.extend({
 
     self.$el.html(self.template());
 
+    this.generateMap();
     return self;
+  },
+
+  generateMap: function () {
+    var map = new window.GMaps({
+      el: this.$('#map')[0],
+      lat: 44.4453485,
+      lng: 26.0586513,
+      width: '100%',
+      height: '20em',
+      zoom: 15
+    });
+    map.addMarker({
+      lat: 44.4453485,
+      lng: 26.0586513
+    })
+    setTimeout(function () {
+      self.map.refresh();
+      self.map.setCenter(44.4453485, 26.0586513);
+    }, 1);
   }
 });
